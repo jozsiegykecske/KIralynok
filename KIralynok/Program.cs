6cbf1c0d77331d347f51b46ecf8082d59122a761
jozsiegykecske <jozsiegykecske@gmail.com>
2021-11-12 10:29:28 +0100

    Add project files.

---
+++ KIralynok/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace KIralynok
+{
+  class Program
+  {
+    static Tabla tabla = new Tabla('#');
+    static void Main(string[] args)
+    {
+      
+      HatodikFeladat();
+      KilencesFeladat();
+      TizedikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void TizedikFeladat()
+    {
+      List<Tabla> tablak = new List<Tabla>();
+      for (int i = 0; i < 64; i++)
+      {
+        tablak.Add(new Tabla('*'));
+        tablak[i].Elhelyez(i+1);
+      }
+      if (File.Exists("tablak64.txt"))
+      {
+        File.Delete("tablak64.txt");
+      }
+      else
+      {
+        StreamWriter ki = new StreamWriter("tablak64.txt");
+        for (int i = 0; i < tablak.Count; i++)
+        {
+          for (int k = 0; k < 8; k++)
+          {
+            ki.WriteLine(tablak[i].Egysor(k));
+            
+          }
+          ki.WriteLine();
+        }
+        ki.Close();
+      }
+      
+    }
+
+    private static void KilencesFeladat()
+    {
+      Console.WriteLine("9.feladat: Üres oszlopok és sorok száma:");
+      Console.WriteLine($"\tOszlopok:{tabla.UresOszlopokSzama}");
+      Console.WriteLine($"\tSorok:{tabla.UresSorokSzama}");
+    }
+
+    private static void HatodikFeladat()
+    {
+      tabla.Elhelyez(5);
+      tabla.Megjelenit();
+    }
+  }
+}
---
+++ KIralynok/Tabla.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KIralynok
+{
+  class Tabla
+  {
+    char[,] T;
+    char UresCella;
+    private int uresoszlopokszama;
+
+    public int UresOszlopokSzama  
+    {
+      get {
+        for (int i = 0; i < 8; i++)
+        {
+          if (UresOszlop(i))
+          {
+            uresoszlopokszama++;
+          }
+        }
+        return uresoszlopokszama; }
+    }
+    private int uressorokszama;
+
+    public int UresSorokSzama
+    {
+      get {
+        for (int i = 0; i < 8; i++)
+        {
+          if (UresSor(i))
+          {
+            uressorokszama++;
+          }
+        }
+        return uressorokszama; }
+    }
+
+    public Tabla(char karakter)
+    {
+      T = new char[8, 8];
+      UresCella = karakter;
+      for (int i = 0; i < 8; i++)
+      {
+        for (int j = 0; j < 8; j++)
+        {
+          T[i, j] = UresCella;
+        }
+      }
+    }
+    public void Megjelenit()
+    {
+      for (int i = 0; i < 8; i++)
+      {
+        for (int j = 0; j < 8; j++)
+        {
+          Console.Write(T[i,j] + " ");
+        }
+        Console.WriteLine();
+      }
+    }
+    public void Elhelyez( int n)
+    {
+      Random random = new Random(Guid.NewGuid().GetHashCode());
+      for (int i = 0; i < n; i++)
+      {
+        int sor, oszlop;
+        do
+        {
+          sor = random.Next(0, 8);
+          oszlop = random.Next(0, 8);
+        } while (T[sor,oszlop]!= UresCella);
+        T[sor, oszlop] = 'K';
+        
+      }
+    }
+    public bool UresSor( int melyikSor)
+    {
+      int szamlalo = 0;
+      while (szamlalo<8 && T[melyikSor,szamlalo]==UresCella)
+      {
+        szamlalo ++;
+      }
+      if (szamlalo<8)
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+    public bool UresOszlop(int melyikOszlop)
+    {
+      int szamlalo = 0;
+      while (szamlalo < 8 && T[szamlalo,melyikOszlop] == UresCella)
+      {
+        szamlalo++;
+      }
+      if (szamlalo < 8)
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+    public string Egysor(int hanyadikoszlop)
+    {
+      string sor = "";
+      for (int i = 0; i < 8; i++)
+      {
+        sor += T[hanyadikoszlop,i] + " ";
+      }
+      return sor;
+    }
+  }
+}
